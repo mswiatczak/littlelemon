bea261e05424b657d572b2375845699b9f272d11
mswiatczak <michalswiatczakk@gmail.com>
2025-10-19 00:20:55 +0200

    fix:button in form

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Button.css";
 
-export default function Button({ text, href, type }) {
+export default function Button({ text, href, type, onClick }) {
   if (href) {
     return (
       <a href={href} className="Button">
@@ -11,7 +11,7 @@ export default function Button({ text, href, type }) {
   }
 
   return (
-    <button type={type || "button"} className="Button">
+    <button type={type || "button"} className="Button" onClick={onClick}>
       {text}
     </button>
   );
--- src/pages/Reservations/components/BookingForm.jsx
+++ src/pages/Reservations/components/BookingForm.jsx
@@ -100,6 +100,7 @@ export default function BookingForm() {
       occasion: "none",
       additionalInfo: "",
     });
+    setErrors({});
     setIsSubmitted(false);
     setConfirmationId("");
   };
